f7ca2c92b4d722cca452e1268654d64da16c265d
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-03-26 01:40:05 -0500

    AWESOME Text working for keybindings

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,59 @@
 exports.activate = function() {
 	console.log('hi');
 	
-	nova.commands.register("editor.moveCursorUp", (editor) => {
+	nova.commands.register("arth.moveCursorUp", (editor) => {
 		editor.moveUp();
 	});
 	
-	nova.commands.register("editor.moveCursorDown", (editor) => {
+	/**
+	 * Editor navigation
+	 */
+	nova.commands.register("arth.moveCursorUp", (editor) => {
+		editor.moveUp();
+	});
+	
+	nova.commands.register("arth.moveCursorDown", (editor) => {
 		editor.moveDown();
 	});
 	
-	nova.commands.register("editor.moveCursorLeft", (editor) => {
+	nova.commands.register("arth.moveCursorLeft", (editor) => {
 		editor.moveLeft();
 	});
 	
-	nova.commands.register("editor.moveCursorRight", (editor) => {
+	nova.commands.register("arth.moveCursorRight", (editor) => {
 		editor.moveRight();
 	});
 	
-	nova.commands.register("editor.moveCursorWordEnd", (editor) => {
+	nova.commands.register("arth.moveCursorWordEnd", (editor) => {
 		editor.moveToEndOfWord();
 	});
 	
-	nova.commands.register("editor.moveCursorWordBeginning", (editor) => {
+	nova.commands.register("arth.moveCursorWordBeginning", (editor) => {
 		editor.moveToBeginningOfWord();
 	});
 	
-	nova.commands.register("editor.moveCursorLineEnd", (editor) => {
+	nova.commands.register("arth.moveCursorLineEnd", (editor) => {
 		editor.moveToEndOfLine();
 	});
 	
-	nova.commands.register("editor.moveCursorLineBeginning", (editor) => {
+	nova.commands.register("arth.moveCursorLineBeginning", (editor) => {
 		editor.moveToBeginningOfLine();
 	});
+	
+	nova.commands.register("arth.moveCursorLineBeginning", (editor) => {
+		editor.moveToBeginningOfLine();
+	});
+	
+	/**
+	 * Selecting
+	 */
+	 nova.commands.register("arth.selectLeft", (editor) => {
+		 editor.selectLeft();
+	 });
+	 
+	 nova.commands.register("arth.selectRight", (editor) => {
+		  editor.selectRight();
+	  });
 }
 
 exports.deactivate = function() {
