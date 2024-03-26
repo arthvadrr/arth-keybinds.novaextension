3d388bf2e67ee901ccd57cd3980caa28144a9d8a
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-03-26 00:26:24 -0500

    including more commands

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,36 @@
 exports.activate = function() {
 	console.log('hi');
 	
+	nova.commands.register("editor.moveCursorUp", (editor) => {
+		editor.moveUp();
+	});
+	
 	nova.commands.register("editor.moveCursorDown", (editor) => {
-		console.log("down");
-		nova.commands.invoke("editor:moveCursorDown");
+		editor.moveDown();
+	});
+	
+	nova.commands.register("editor.moveCursorLeft", (editor) => {
+		editor.moveLeft();
+	});
+	
+	nova.commands.register("editor.moveCursorRight", (editor) => {
+		editor.moveRight();
+	});
+	
+	nova.commands.register("editor.moveCursorWordEnd", (editor) => {
+		editor.moveToEndOfWord();
+	});
+	
+	nova.commands.register("editor.moveCursorWordBeginning", (editor) => {
+		editor.moveToBeginningOfWord();
+	});
+	
+	nova.commands.register("editor.moveCursorLineEnd", (editor) => {
+		editor.moveToEndOfLine();
+	});
+	
+	nova.commands.register("editor.moveCursorLineBeginning", (editor) => {
+		editor.moveToBeginningOfLine();
 	});
 }
 
