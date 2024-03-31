5ecca9a0e48ba2275f360e77df34f5cf0c7c9ee0
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-03-31 01:05:17 -0500

    Added png and changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+# Changelog ## 0.0.1 Writing keybinds
--- scripts/main.js
+++ scripts/main.js
@@ -1,97 +1,87 @@
-exports.activate = function() {
-	console.log('hi');
+exports.activate = function () {
+  nova.commands.register("arth.moveCursorUp", (editor) => {
+    editor.moveUp();
+  });
 
-	nova.commands.register("arth.moveCursorUp", (editor) => {
-		editor.moveUp();
-	});
+  /**
+   * Editor navigation
+   */
+  nova.commands.register("arth.moveCursorUp", (editor) => {
+    editor.moveUp();
+  });
 
-	/**
-	 * Editor navigation
-	 */
-	nova.commands.register("arth.moveCursorUp", (editor) => {
-		editor.moveUp();
-	});
+  nova.commands.register("arth.moveCursorDown", (editor) => {
+    editor.moveDown();
+  });
 
-	nova.commands.register("arth.moveCursorDown", (editor) => {
-		editor.moveDown();
-	});
+  nova.commands.register("arth.moveCursorLeft", (editor) => {
+    editor.moveLeft();
+  });
 
-	nova.commands.register("arth.moveCursorLeft", (editor) => {
-		editor.moveLeft();
-	});
+  nova.commands.register("arth.moveCursorRight", (editor) => {
+    editor.moveRight();
+  });
 
-	nova.commands.register("arth.moveCursorRight", (editor) => {
-		editor.moveRight();
-	});
+  nova.commands.register("arth.moveCursorWordEnd", (editor) => {
+    editor.moveToEndOfWord();
+  });
 
-	nova.commands.register("arth.moveCursorWordEnd", (editor) => {
-		editor.moveToEndOfWord();
-	});
+  nova.commands.register("arth.moveCursorWordBeginning", (editor) => {
+    editor.moveToBeginningOfWord();
+  });
 
-	nova.commands.register("arth.moveCursorWordBeginning", (editor) => {
-		editor.moveToBeginningOfWord();
-	});
+  nova.commands.register("arth.moveCursorLineEnd", (editor) => {
+    editor.moveToEndOfLine();
+  });
 
-	nova.commands.register("arth.moveCursorLineEnd", (editor) => {
-		editor.moveToEndOfLine();
-	});
+  nova.commands.register("arth.moveCursorLineBeginning", (editor) => {
+    editor.moveToBeginningOfLine();
+  });
 
-	nova.commands.register("arth.moveCursorLineBeginning", (editor) => {
-		editor.moveToBeginningOfLine();
-	});
+  nova.commands.register("arth.moveCursorLineBeginning", (editor) => {
+    editor.moveToBeginningOfLine();
+  });
 
-	nova.commands.register("arth.moveCursorLineBeginning", (editor) => {
-		editor.moveToBeginningOfLine();
-	});
+  nova.commands.register("arth.moveCursorUpTen", (editor) => {
+    editor.moveUp(10);
+  });
 
-	nova.commands.register("arth.moveCursorUpTen", (editor) => {
-		editor.moveUp(10);
-	});
+  nova.commands.register("arth.moveCursorDownTen", (editor) => {
+    editor.moveDown(10);
+  });
 
-	nova.commands.register("arth.moveCursorDownTen", (editor) => {
-		editor.moveDown(10);
-	});
+  /**
+   * Selecting
+   */
+  nova.commands.register("arth.selectLeft", (editor) => {
+    editor.selectLeft();
+  });
 
-	/**
-	 * Selecting
-	 */
-	nova.commands.register("arth.selectLeft", (editor) => {
-		editor.selectLeft();
-	});
+  nova.commands.register("arth.selectRight", (editor) => {
+    editor.selectRight();
+  });
 
-	nova.commands.register("arth.selectRight", (editor) => {
-		editor.selectRight();
-	})
-	
-	nova.commands.register("arth.selectToBeginningOfWord", (editor) => {
-		editor.selectToBeginningOfWord();
-	});
+  nova.commands.register("arth.selectToBeginningOfWord", (editor) => {
+    editor.selectToBeginningOfWord();
+  });
 
-	nova.commands.register("arth.selectToEndOfWord", (editor) => {
-		editor.selectToEndOfWord();
-	});
-	
-	nova.commands.register("arth.selectToBeginningOfLine", (editor) => {
-		editor.selectToBeginningOfLine();
-	});
-	
-	nova.commands.register("arth.selectToEndOfLine", (editor) => {
-		editor.selectToEndOfLine();
-	});
-	
-	nova.commands.register("arth.selectLineUp", (editor) => {
-		editor.selectUp();
-	});
-	
-	nova.commands.register("arth.selectLineDown", (editor) => {
-		editor.selectDown();
-	});
-	
-	/**
-	 * Text Edit
-	 */
-}
+  nova.commands.register("arth.selectToEndOfWord", (editor) => {
+    editor.selectToEndOfWord();
+  });
 
-exports.deactivate = function() {
-	// Clean up state before the extension is deactivated
-}
+  nova.commands.register("arth.selectToBeginningOfLine", (editor) => {
+    editor.selectToBeginningOfLine();
+  });
+
+  nova.commands.register("arth.selectToEndOfLine", (editor) => {
+    editor.selectToEndOfLine();
+  });
+
+  nova.commands.register("arth.selectLineUp", (editor) => {
+    editor.selectUp();
+  });
+
+  nova.commands.register("arth.selectLineDown", (editor) => {
+    editor.selectDown();
+  });
+};
